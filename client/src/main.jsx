f90f48d482ba90e6c60d8469ed5965a237540af4
Monique Wakefield <monique_wakefield@yahoo.com>
2025-06-08 11:13:52 -0500

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App.jsx';
-import Login from './Login.jsx';
-import Register from './Register.jsx';
-import Dashboard from './Dashboard.jsx';
+import Login from './Login';
+import Register from './Register';
+import Dashboard from './Dashboard';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
