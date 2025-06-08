9af9d4320f97d1ceeac2258b9a564bdb0de78276
Monique Wakefield <monique_wakefield@yahoo.com>
2025-06-08 11:54:44 -0500

    updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './Login';
-import Register from './Register';
-import Dashboard from './Dashboard';
+import App from './App.jsx';
+import Login from './Login.jsx';
+import Register from './Register.jsx';
+import Dashboard from './Dashboard.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<App />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/dashboard/:username" element={<Dashboard />} />
     </Routes>
