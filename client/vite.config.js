11369fc92da3ffd3772412a6b413752c2816787b
Monique Wakefield <monique_wakefield@yahoo.com>
2025-06-08 11:44:25 -0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/register': 'http://localhost:5000',
+      '/login': 'http://localhost:5000',
+      '/questions': 'http://localhost:5000'
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: '/index.html'
+    }
+  }
+});
