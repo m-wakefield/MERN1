ef438ec6a2d664f7722b1d8c837c3200ac821116
Monique Wakefield <monique_wakefield@yahoo.com>
2025-06-08 11:00:15 -0500

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App.jsx';
+import Login from './Login.jsx';
+import Register from './Register.jsx';
+import Dashboard from './Dashboard.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard/:username" element={<Dashboard />} />
+    </Routes>
+  </BrowserRouter>
+);
